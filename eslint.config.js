ed30bc73d5bf27d0b9ee7d16e7c019a1800f9936
Akhil roczz <b.akhil667@gmail.com>
2026-06-13 12:12:55 +0530

    next commit

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,39 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist','vite.config.ts']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
+    plugins:{
+      react,
+      '@typescript-eslint': tseslint.plugin,
+      prettier: prettierPlugin,
+    },
+    rules:{
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': "warn"
+    },
+    settings:{
+      react:{
+        version: 'detect',
+      }
+    },
     languageOptions: {
       globals: globals.browser,
     },
