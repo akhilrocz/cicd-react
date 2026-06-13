ea65adef04b828f6adfc835a0c39f463988f61b8
Akhil roczz <b.akhil667@gmail.com>
2026-06-13 12:47:38 +0530

    chore: next commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,25 @@
+module.exports ={
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-min-length': [2, 'always', 10],
+        'header-max-length': [2, 'always'],
+        'header-case-end-period':[2,'always'],
+        'header-case-start-capital':[2,'always']
+    },
+    plugins: [
+        {
+            rules:{
+                'header-case-start-capital': ({raw}) => {
+                    return [/^[A-Z]/.test(raw),
+                    'Commit message must start with capital letter'
+                    ];
+                },
+                'header-case-end-period': ({header}) => {
+                    return [!/\.$/.test(header),
+                    'Commit message must end with period'
+                    ];
+                }
+            }
+        }
+    ]
+}
