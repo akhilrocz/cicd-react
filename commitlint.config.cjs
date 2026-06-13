95fbfc998e90151b9af4cb20d01292236c5ae964
Akhil roczz <b.akhil667@gmail.com>
2026-06-13 12:58:03 +0530

    fix: fixed issues

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,23 +1,7 @@
-module.exports ={
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'header-min-length': [2, 'always', 10],
-        'header-max-length': [2, 'always', 72],
-    },
-    plugins: [
-        {
-            rules:{
-                'header-case-start-capital': ({raw}) => {
-                    return [/^[A-Z]/.test(raw),
-                    'Commit message must start with capital letter'
-                    ];
-                },
-                'header-case-end-period': ({header}) => {
-                    return [!/\.$/.test(header),
-                    'Commit message must end with period'
-                    ];
-                }
-            }
-        }
-    ]
-}
+module.exports = {
+extends: ['@commitlint/config-conventional'],
+rules: {
+'header-min-length': [2, 'always', 10],
+'header-max-length': [2, 'always', 72],
+},
+};
