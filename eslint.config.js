21c6f8a2becead740388510266521f942214d531
Akhil roczz <b.akhil667@gmail.com>
2026-06-13 12:22:52 +0530

    next commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import prettierPlugin from 'eslint-plugin-prettier'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist','vite.config.ts']),
+  globalIgnores(['dist', 'vite.config.ts']),
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      react.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-      eslintConfigPrettier,
-    ],
-    plugins:{
+
+    plugins: {
       react,
-      '@typescript-eslint': tseslint.plugin,
+      'react-refresh': reactRefresh,
       prettier: prettierPlugin,
     },
-    rules:{
-      'prettier/prettier': 'error',
-      'react/react-in-jsx-scope': 'off',
-      'react-refresh/only-export-components': "warn"
-    },
-    settings:{
-      react:{
-        version: 'detect',
-      }
-    },
+
     languageOptions: {
       globals: globals.browser,
     },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': 'warn',
+    },
   },
-])
+]);
